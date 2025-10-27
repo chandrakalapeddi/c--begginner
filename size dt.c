d9cebbe6972a56e02004042c47b90b639b166934
chandrakalapeddi <chandrakalapeddi192@gmail.com>
2025-10-27 21:31:16 +0530

    Add program to display sizes of data types

---
+++ size dt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+float b;
+char c;
+double d;
+printf(" size of int=%1u bytes\n", size of (a));
+printf(" size of float=%1u bytes\n", size of (b));
+printf(" size of char=%1u bytes\n", size of (c));
+printf(" size of double=%1u bytes\n", size of (d));
+return 0;
+}
