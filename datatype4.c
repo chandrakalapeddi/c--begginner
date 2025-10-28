72fdd29db0fae11a2384ffa219bba2d83e8a1b0f
chandrakalapeddi <chandrakalapeddi192@gmail.com>
2025-10-28 10:50:33 +0530

    Add basic arithmetic operations in datatype4.c
    Demonstrate basic arithmetic operations on an integer.

---
+++ datatype4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a=10;
+printf("%d\n", a);
+a+=4;
+printf("%d\n", a);
+a-=2;
+printf("%d\n", a);
+a*=2;
+printf("%d\n", a);
+a/=3;
+printf("%d\n", a);
+a%=1;
+printf("%d\n", a);
+return 0;
+}
