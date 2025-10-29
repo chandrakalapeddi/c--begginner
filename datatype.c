6ea8af0562e6a9db6722be5a1a9e766a3714261f
chandrakalapeddi <chandrakalapeddi192@gmail.com>
2025-10-29 06:10:36 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int age=20;
+float height=5.8;
+double weight=65.43;
+char grade='A';
+printf("age %d\n", age);
+printf("height %d\n",height);
+printf("weight %d\n", weight);
+printf("grade %d\n", grade);
+return 0;
+}
