2ec823e421bf75e4f6df62d24514dd2b8e638ea6
chandrakalapeddi <chandrakalapeddi192@gmail.com>
2025-10-29 07:10:40 +0530

    Add even/odd check for user input value X

---
+++ if else 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int X;
+	printf("enter value for X");
+	scanf("%d",&X);
+	if(X%2==0)
+	{
+		printf("X is even");
+	}
+	else
+	{
+		printf("X is odd");
+    }
+    return 0;
+}
