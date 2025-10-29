6ee1ae4614763bc061f226019dba75fde994c146
chandrakalapeddi <chandrakalapeddi192@gmail.com>
2025-10-29 07:45:35 +0530

    Create nested if loop2.c

---
+++ nested if loop2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int A,B,C;
+	printf("enter trhee numbers");
+	scanf("%d %d %d", &A, &B, &C);
+	if(A>=B)
+	{
+		if (A>=C)
+		printf("%d is the largest number.", A);
+	else	
+		printf("%d is the largest number.", C);	
+	}
+	else
+	{
+		if (B>=C)
+			printf("%d is the largest number.", B);
+	    else
+	    	printf("%d is the largest number.", C);
+	} 
+	return 0;
+}
