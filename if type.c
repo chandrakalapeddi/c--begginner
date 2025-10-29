2da1144d52d52ef2007be824a81b4b2502575305
chandrakalapeddi <chandrakalapeddi192@gmail.com>
2025-10-29 07:09:37 +0530

    Create if type.c

---
+++ if type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a value");
+	scanf("%d", &a);
+	if(a);
+{
+		printf("it is inside");
+	}
+	printf("the value of a is:%d", a);
+	printf("it is outside");
+	return 0;
+}
